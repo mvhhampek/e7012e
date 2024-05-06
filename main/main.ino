bb6952f49b685130e0a233554761a7bbccb872bb
mvhhampek <59012814+mvhhampek@users.noreply.github.com>
2024-05-06 11:20:07 +0200

    asdasdasda

--- main/main.ino
+++ main/main.ino
@@ -7,10 +7,22 @@ const byte HALL_INTERRUPT_PIN = 12;
 const byte MOTOR_PIN = 8;
 const byte SERVO_PIN = 5;
 
+
+const byte LEFT_PING_PIN = 26;
+const byte LEFT_ECHO_PIN = 27;
+
+const byte RIGHT_PING_PIN = 32;
+const byte RIGHT_ECHO_PIN = 33;
+
+
+
 // Wheel circumference in meters
 const float WHEEL_CIRCUMFERENCE = 0.2042; 
 const int  NUM_MAGNETS = 4;
 
+
+volatile float left_distance = 0;
+volatile float right_distance = 0; 
 
 // Counter for hall interrupts
 volatile int hall_counter = 0;     
@@ -34,11 +46,12 @@ int motor_speed = 1500;  // 1500 stilla, 2000 max frammåt, 1000 max backåt
 int steering_angle = 70; // mellan 40 och 100 typ
 
 int speed_ref = 0;
-int old_speed_ref = -1;
-int old_steering_angle = -1;
+int old_speed_ref = 0;
+int old_steering_angle = 70;
 
 // Forward speed computed by encoder
 float current_vel = 0;
+volatile float prev_left = 0;
 
 // Time between pulses, used to calculate speed
 volatile int last_time = 0;
@@ -67,9 +80,18 @@ void setup() {
     Serial.println("setup");
     steering_servo.write(70);
     motor_servo.writeMicroseconds(motor_speed);
+  
+
+    pinMode(LEFT_PING_PIN, OUTPUT);
+    pinMode(LEFT_ECHO_PIN, INPUT);
+
+    
+    pinMode(RIGHT_PING_PIN, OUTPUT);
+    pinMode(RIGHT_ECHO_PIN, INPUT);
+
+    
+
     delay(1000);    
-    motor_servo.writeMicroseconds(1500);
-    delay(1000);
 }
 
 void loop() {
@@ -81,11 +103,10 @@ void loop() {
     }
     motor_servo.writeMicroseconds(u);
 
-    if (current_time%1000==0) {
-        Serial.print(current_vel);
-        Serial.println(" m/s");
-    }
-
+    
+    Serial.print(current_vel);
+    Serial.println(" m/s");
+    
     // uppdaterar delta_time
     if (update_flag) {
       updateDelta();
@@ -101,41 +122,62 @@ void loop() {
       pid.update_velocity(current_vel);
     }
 
+    //left_distance = getDistance(LEFT_PING_PIN, LEFT_ECHO_PIN);
+    //right_distance = getDistance(RIGHT_PING_PIN, RIGHT_ECHO_PIN);
+
+    //right_distance = getDistance(RIGHT_PING_PIN, RIGHT_ECHO_PING);
+    //forward_distance = getDistance(FORWARD_PING_PIN, FORWARD_ECHO_PIN);
+    
+    //Serial.print("Right distance: ");
+    //Serial.println(right_distance);
+    //Serial.print("Forward distance: ");
+    //Serial.println(forward_distance);
+
+
+
+
     if (Serial.available() > 0) { 
-        input = Serial.readStringUntil('\n');
-        int split_index = input.indexOf('-');
-        steering_angle = input.substring(0,split_index).toInt();
-        speed_ref = input.substring(split_index+1).toInt();  
-
-        if (old_speed_ref != speed_ref || old_steering_angle != steering_angle){
-            pid.set_velocity(int_input);
-            steering_servo.write(steering_angle);
-            old_speed_ref = speed_ref;
-            old_steering_angle = steering_angle;
-        }
-
-        /*
-        // change motor speed
-        if (int_input > 1000 && int_input < 2000) {
-            motor_speed = int_input;
-            motor_servo.writeMicroseconds(motor_speed);
-            Serial.print("Set motorspeed to ");
-            Serial.println(motor_speed);
-        } //change steering angle
-        else if (int_input > 40 && int_input < 100) {
-            steering_angle = int_input;
-            steering_servo.write(steering_angle);
-            Serial.print("Set steering angle to ");
-            Serial.println(steering_angle);
-        } //change ref
-        else if (int_input > -5 && int_input < 5) {
-            
-            Serial.print("Set ref to ");
-        }  */
-
+        setSpeedAndAngle();
     }    
 }
 
+void setSpeedAndAngle(){
+    input = Serial.readStringUntil('\n');
+    int split_index = input.indexOf('-');
+    steering_angle = input.substring(0,split_index).toInt();
+    speed_ref = input.substring(split_index+1).toInt();  
+
+    if (old_speed_ref != speed_ref || old_steering_angle != steering_angle){
+        pid.set_velocity(speed_ref);
+        steering_servo.write(steering_angle);
+        old_speed_ref = speed_ref;
+        old_steering_angle = steering_angle;
+        Serial.println(input);
+    }
+
+}
+
+
+long getDistance(byte trigger_pin, byte echo_pin){
+    long duration, cm;
+    
+    digitalWrite(trigger_pin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigger_pin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigger_pin, LOW);
+    //pinMode(echo_pin, INPUT);
+    duration = pulseIn(echo_pin, HIGH);
+    cm = microsecondsToCentimeters(duration);
+
+    //delay(100);
+    return cm;
+
+}
+
+long microsecondsToCentimeters(long microseconds){
+  return microseconds/29/2;
+}
 
 // Updaterar delta_time
 void updateDelta(){
@@ -164,3 +206,26 @@ void hallISR(){
     update_flag=true;
     hall_counter++;
 }
+
+
+
+
+
+        /*
+        // change motor speed
+        if (int_input > 1000 && int_input < 2000) {
+            motor_speed = int_input;
+            motor_servo.writeMicroseconds(motor_speed);
+            Serial.print("Set motorspeed to ");
+            Serial.println(motor_speed);
+        } //change steering angle
+        else if (int_input > 40 && int_input < 100) {
+            steering_angle = int_input;
+            steering_servo.write(steering_angle);
+            Serial.print("Set steering angle to ");
+            Serial.println(steering_angle);
+        } //change ref
+        else if (int_input > -5 && int_input < 5) {
+            
+            Serial.print("Set ref to ");
+        }  */
